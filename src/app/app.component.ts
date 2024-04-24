b78d31b0df35d942d443ca050f7038c32c963b74
PedroMartinezGC <pedromartinezgcicuendez@gmail.com>
2024-04-24 23:15:08 +0200

    feat(header-section): add sliders for the header section and about me section. PF-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ScreenSizeService } from './services/screen-size.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,17 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'PortfolioII';
+
+  constructor(private screenSizeService: ScreenSizeService) {}
+
+  ngOnInit(): void {
+    this.screenSizeService.setScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.screenSizeService.setScreenSize();
+  }
 }
--- src/app/public/dashboard/dashboard.component.ts
+++ src/app/public/dashboard/dashboard.component.ts
@@ -1,23 +1,47 @@
 import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { HamburguerButtonComponent } from '../../components/hamburguer-button/hamburguer-button.component';
+import { ScreenSizeEnum, ScreenSizeService } from '../../services/screen-size.service';
+import { NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HamburguerButtonComponent],
+  imports: [HamburguerButtonComponent, NgIf],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit, AfterViewInit {
 
   isMenuOpened: boolean = false;
+  showMobileMenu: boolean = false;
 
   @ViewChild('menuDropdown') menuDropdown: ElementRef;
   @ViewChild('navMenu') navMenu: ElementRef;
   @ViewChild('hamburguerButton') hamburguerButton: HamburguerButtonComponent;
+  @ViewChild('slideTop') slideTop: ElementRef;
+  @ViewChild('sliderTopBox') sliderTopBox: ElementRef;
+  @ViewChild('slideBottom') slideBottom: ElementRef;
+  @ViewChild('sliderBottomBox') sliderBottomBox: ElementRef;
 
-  constructor( private renderer2: Renderer2 )  {
+  constructor(private renderer2: Renderer2,
+              private screenSizeService: ScreenSizeService )  {
+  }
+
+  ngOnInit() {
+    this.screenSizeService.mediaDevice$.subscribe((mediaDevice: ScreenSizeEnum) => {
+      console.log(mediaDevice)
+      this.showMobileMenu = mediaDevice === ScreenSizeEnum.Mobile || mediaDevice === ScreenSizeEnum.Tablet ? true : false;
+      console.log(this.showMobileMenu)
+    });
+  }
+
+  ngAfterViewInit(): void {
+    const copySliderTop = this.slideTop.nativeElement.cloneNode(true);
+    const copySliderBottom = this.slideBottom.nativeElement.cloneNode(true);
+
+    this.sliderTopBox.nativeElement.appendChild(copySliderTop);
+    this.sliderBottomBox.nativeElement.appendChild(copySliderBottom);
   }
 
   /**
@@ -40,6 +64,11 @@ export class DashboardComponent {
     this.renderer2.setStyle(this.menuDropdown.nativeElement, 'top', (-1 * navMenuHeight) + 'px');
     this.renderer2.setStyle(this.navMenu.nativeElement, 'border-bottom', '1px solid #f6ebe5');
     setTimeout(() => { this.isMenuOpened = false }, 300);
+  }
+
+  private copySliderContent(slide: ElementRef, sliderBox: ElementRef) {
+    const copy = slide.nativeElement.cloneNode(true);
+    sliderBox.nativeElement.appendChild(copy);
   }
 
 }
