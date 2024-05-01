d88f1bd7c05bf04b3fca0ca1e104dc8800312de0
PedroMartinezGC <pedromartinezgcicuendez@gmail.com>
2024-05-01 19:45:24 +0200

    feat(contact): add contact section, infinite marquee component and magnetic button component. PF-5

--- src/app/public/dashboard/dashboard.component.ts
+++ src/app/public/dashboard/dashboard.component.ts
@@ -1,74 +1,82 @@
-import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { HamburguerButtonComponent } from '../../components/hamburguer-button/hamburguer-button.component';
 import { ScreenSizeEnum, ScreenSizeService } from '../../services/screen-size.service';
 import { NgIf } from '@angular/common';
+import { InfiniteMarqueeComponent } from '../../components/infinite-marquee/infinite-marquee.component';
+import { MagneticButtonComponent } from '../../components/magnetic-button/magnetic-button.component';
+
+enum PageSectionsEnum {
+	Experience = 'Experience',
+	Contact = 'Contact'
+}
 
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [HamburguerButtonComponent, NgIf],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+	selector: 'app-dashboard',
+	standalone: true,
+	imports: [HamburguerButtonComponent, InfiniteMarqueeComponent, MagneticButtonComponent, NgIf],
+	templateUrl: './dashboard.component.html',
+	styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent implements OnInit, AfterViewInit {
+export class DashboardComponent implements OnInit {
 
-  isMenuOpened: boolean = false;
-  showMobileMenu: boolean = false;
+	isMenuOpened: boolean = false;
+	showMobileMenu: boolean = false;
+	pageSectionsEnum = PageSectionsEnum;
 
-  @ViewChild('menuDropdown') menuDropdown: ElementRef;
-  @ViewChild('navMenu') navMenu: ElementRef;
-  @ViewChild('hamburguerButton') hamburguerButton: HamburguerButtonComponent;
-  @ViewChild('slideTop') slideTop: ElementRef;
-  @ViewChild('sliderTopBox') sliderTopBox: ElementRef;
-  @ViewChild('slideBottom') slideBottom: ElementRef;
-  @ViewChild('sliderBottomBox') sliderBottomBox: ElementRef;
+	@ViewChild('menuDropdown') menuDropdown: ElementRef;
+	@ViewChild('navMenu') navMenu: ElementRef;
+	@ViewChild('hamburguerButton') hamburguerButton: HamburguerButtonComponent;
+	@ViewChild('experienceSection') experienceSection: ElementRef;
+	@ViewChild('contactSection') contactSection: ElementRef;
 
-  constructor(private renderer2: Renderer2,
-              private screenSizeService: ScreenSizeService )  {
-  }
+	constructor(private renderer2: Renderer2,
+		private screenSizeService: ScreenSizeService) {
+	}
 
-  ngOnInit() {
-    this.screenSizeService.mediaDevice$.subscribe((mediaDevice: ScreenSizeEnum) => {
-      console.log(mediaDevice)
-      this.showMobileMenu = mediaDevice === ScreenSizeEnum.Mobile || mediaDevice === ScreenSizeEnum.Tablet ? true : false;
-      console.log(this.showMobileMenu)
-    });
-  }
+	ngOnInit() {
+		this.screenSizeService.mediaDevice$.subscribe((mediaDevice: ScreenSizeEnum) => {
+			this.showMobileMenu = mediaDevice === ScreenSizeEnum.Mobile || mediaDevice === ScreenSizeEnum.Tablet ? true : false;
+		});
+	}
 
-  ngAfterViewInit(): void {
-    const copySliderTop = this.slideTop.nativeElement.cloneNode(true);
-    const copySliderBottom = this.slideBottom.nativeElement.cloneNode(true);
+	/**
+	 * Open and closes menu dropdown; When closes, it hides the dropdown under navMenu.
+	 * @param event 
+	 */
+	toggleDropdownMenu() {
+		const navMenuHeight = this.navMenu.nativeElement.clientHeight;
+		this.isMenuOpened ? this.closeMenu(navMenuHeight) : this.openMenu(navMenuHeight);
+		this.hamburguerButton.onHamburguerClick();
+	}
 
-    this.sliderTopBox.nativeElement.appendChild(copySliderTop);
-    this.sliderBottomBox.nativeElement.appendChild(copySliderBottom);
-  }
+	openMenu(navMenuHeight: number) {
+		this.renderer2.setStyle(this.menuDropdown.nativeElement, 'top', navMenuHeight + 'px');
+		this.renderer2.setStyle(this.navMenu.nativeElement, 'border-bottom', '1px solid #2c2b2b');
+		setTimeout(() => { this.isMenuOpened = true }, 300);
+	}
 
-  /**
-   * Open and closes menu dropdown; When closes, it hides the dropdown under navMenu.
-   * @param event 
-   */
-  toggleDropdownMenu() {
-    const navMenuHeight = this.navMenu.nativeElement.clientHeight;
-    this.isMenuOpened ? this.closeMenu(navMenuHeight) : this.openMenu(navMenuHeight);
-    this.hamburguerButton.onHamburguerClick();
-  }
+	closeMenu(navMenuHeight: number) {
+		this.renderer2.setStyle(this.menuDropdown.nativeElement, 'top', (-1 * navMenuHeight) + 'px');
+		this.renderer2.setStyle(this.navMenu.nativeElement, 'border-bottom', '1px solid #f6ebe5');
+		setTimeout(() => { this.isMenuOpened = false }, 300);
+	}
 
-  openMenu(navMenuHeight: number) {
-    this.renderer2.setStyle(this.menuDropdown.nativeElement, 'top', navMenuHeight + 'px');
-    this.renderer2.setStyle(this.navMenu.nativeElement, 'border-bottom', '1px solid #2c2b2b');
-    setTimeout(() => { this.isMenuOpened = true }, 300);
-  }
+	scrollToTop() {
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+	}
 
-  closeMenu(navMenuHeight: number) {
-    this.renderer2.setStyle(this.menuDropdown.nativeElement, 'top', (-1 * navMenuHeight) + 'px');
-    this.renderer2.setStyle(this.navMenu.nativeElement, 'border-bottom', '1px solid #f6ebe5');
-    setTimeout(() => { this.isMenuOpened = false }, 300);
-  }
-
-  private copySliderContent(slide: ElementRef, sliderBox: ElementRef) {
-    const copy = slide.nativeElement.cloneNode(true);
-    sliderBox.nativeElement.appendChild(copy);
-  }
+	scrollToSection(sectionEnum: PageSectionsEnum) {
+		let section: any;
+		switch (sectionEnum) {
+			case PageSectionsEnum.Experience:
+				section = this.experienceSection.nativeElement;
+				break;
+			case PageSectionsEnum.Contact:
+				section = this.contactSection.nativeElement;
+				break;
+		}
+		section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+	}
 
 }
